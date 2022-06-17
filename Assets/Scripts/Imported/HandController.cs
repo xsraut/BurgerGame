c53cb14a2c2f56bceb942e9ac7b2bdf715518f11
xsraut <sourabhdec121998@gmail.com>
2022-06-17 23:47:23 +0530

    Added Constraint For Cursor
    The cursor ("Hand Sprite") contains a script HandController.cs which is responsible for controlling the cursor based on the inputs.
    
    A constraint for keeping the cursor within the viewport is added.

--- Assets/Scripts/Imported/HandController.cs
+++ Assets/Scripts/Imported/HandController.cs
@@ -18,15 +18,27 @@ public class HandController : MonoBehaviour
 
     public bool pick;
 
+    int screenSizeH;
+    int screenSizeV;
+    Camera cam;
+    [SerializeField]
+    Vector3 cursorSizeOffset;
     // Start is called before the first frame update
     void Start()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
+
+        screenSizeH = Screen.width;
+        screenSizeV = Screen.height;
+
+        cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(cam.WorldToScreenPoint(transform.position));
+
         if (Input.GetKey(pickAndDrop))
         {
             _spriteRenderer.sprite = GrabSprite;
@@ -38,11 +50,11 @@ public class HandController : MonoBehaviour
             if (pick) pick = false;
         }
 
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) && cam.WorldToScreenPoint(transform.position).x > (0 + cursorSizeOffset.x))
         {
             currPos.x = -1;
         }
-        else if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D) && cam.WorldToScreenPoint(transform.position).x < (screenSizeH - cursorSizeOffset.y))
         {
             currPos.x = 1;
         }
@@ -51,11 +63,11 @@ public class HandController : MonoBehaviour
             currPos.x = 0;
         }
 
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W) && cam.WorldToScreenPoint(transform.position).y < (screenSizeV - cursorSizeOffset.y))
         {
             currPos.y = 1;
         }
-        else if (Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(KeyCode.S) && cam.WorldToScreenPoint(transform.position).y > (0 + cursorSizeOffset.y))
         {
             currPos.y = -1;
         }
